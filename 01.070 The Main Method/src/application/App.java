145ba4a4ea7025f7a628e18a9b4bd2cc2ac9df0e
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2020-11-26 21:19:44 -0600

    Java started ..

--- 01.070 The Main Method/src/application/App.java
+++ 01.070 The Main Method/src/application/App.java
@@ -6,6 +6,7 @@ public class App {
 		System.out.println("Hello World!");
 		System.out.println("This is another line of text.");
 		System.out.println("Final line of text.");
+		System.out.println("Hello World");
 	}
 
 }
