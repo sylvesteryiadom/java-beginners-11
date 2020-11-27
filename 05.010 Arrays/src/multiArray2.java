f3bde7902bfb144ab1228b251a049d3e0cd60fba
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2020-11-27 00:56:01 -0600

    Multi-dimensional arrays

--- 05.010 Arrays/src/multiArray2.java
+++ 05.010 Arrays/src/multiArray2.java
@@ -5,11 +5,10 @@ public class multiArray2 {
 		String[][] PetCarComp = { { "dog", "cat" }, { "bmw", "benz" }, { "apple", "google" } };
 
 		// looping through multi-dimensional arrays
-		for (int i = 0; i < PetCarComp.length; i++) {
-			String[] singleArrays = PetCarComp[i];
-//			System.out.println(Arrays.toString(PetCarComp[i]));
-			for (int j = 0; j < singleArrays.length; j++) {
-				System.out.printf("%s \t", singleArrays[j]);
+
+		for (int i = 0; i < PetCarComp.length; i++) { // using i to loop through FOR individual arrays ["dog","cat"]
+			for (int j = 0; j < PetCarComp[i].length; j++) { // using j to loop through FOR individual elements
+				System.out.printf("%s \t", PetCarComp[i][j]);
 			}
 			System.out.println();
 		}
---
+++ 05.010 Arrays/src/multiArrayExercise.java
@@ -0,0 +1,7 @@
+
+public class multiArrayExercise {
+	public static void main(String[] args) {
+
+	}
+
+}
